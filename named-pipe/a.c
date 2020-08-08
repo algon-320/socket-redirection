f548f12c096a1d2a8246ed52412c2bad42e01bc0
algon-320 <algon.0320@gmail.com>
2020-08-08 22:44:40 +0900

    use poll instead of select

--- named-pipe/a.c
+++ named-pipe/a.c
@@ -1,15 +1,10 @@
 #include <fcntl.h>
+#include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/select.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-
-int max(int a, int b) {
-  if (a > b) return a;
-  return b;
-}
 
 // call write syscall repeatedly until entire bytes are written.
 ssize_t write_all(int fd, char *buf, size_t count) {
@@ -28,64 +23,56 @@ int redirect(int a2b, int b2a) {
   const size_t BUF_LEN = 1024;
 
   char buf[BUF_LEN + 1];
-  fd_set readfds;
-  int stdin_closed = 0, b2a_closed = 0;
+  struct pollfd pollfds[2];
+  int idx[2], src_fd[2], dst_fd[2], closed[2] = {0};
+  int nfd;
 
-  while (!b2a_closed) {
-    FD_ZERO(&readfds);
-    FD_SET(STDIN_FILENO, &readfds);
-    FD_SET(b2a, &readfds);
+  src_fd[0] = STDIN_FILENO;
+  dst_fd[0] = a2b;
+  src_fd[1] = b2a;
+  dst_fd[1] = STDOUT_FILENO;
 
-    int ret = select(max(STDIN_FILENO, b2a) + 1, &readfds, NULL, NULL, NULL);
-    // when some error happen
-    if (ret == -1) {
-      perror("select");
-      return 1;
-    }
-
-    // read bytes from stdin and write them to a2b
-    if (!stdin_closed && FD_ISSET(STDIN_FILENO, &readfds)) {
-      ssize_t nread, nwrite;
-      for (;;) {
-        nread = read(STDIN_FILENO, buf, BUF_LEN);
-        if (nread == -1) {
-          perror("read stdin");
-          return 2;
-        }
-        if (nread == 0) {  // EOF
-          stdin_closed = 1;
-          break;
-        }
-
-        if (write_all(a2b, buf, nread) == -1) {
-          perror("write a2b");
-          return 3;
-        }
-
-        if (nread < BUF_LEN) break;
+  while (!closed[1]) {
+    nfd = 0;
+    for (int i = 0; i < 2; i++) {
+      if (!closed[i]) {
+        pollfds[nfd].fd = src_fd[i];
+        pollfds[nfd].events = POLLIN;
+        pollfds[nfd].revents = 0;
+        idx[nfd] = i;
+        nfd += 1;
       }
     }
 
-    // read bytes from b2a and write them to stdout
-    if (!b2a_closed && FD_ISSET(b2a, &readfds)) {
-      ssize_t nread, nwrite;
-      for (;;) {
-        nread = read(b2a, buf, BUF_LEN);
-        if (nread == -1) {
-          perror("read b2a");
-          return 2;
-        }
-        if (nread == 0) {  // EOF
-          b2a_closed = 1;
-          break;
-        }
+    int ret = poll(pollfds, nfd, -1);
+    // when some error happen
+    if (ret == -1) {
+      perror("poll");
+      return 1;
+    }
 
-        if (write_all(STDOUT_FILENO, buf, nread) == -1) {
-          perror("write stdout");
-          return 3;
+    for (int i = 0; i < nfd; i++) {
+      if (!closed[idx[i]] && (pollfds[i].revents & POLLIN)) {
+        ssize_t nread, nwrite;
+        for (;;) {
+          nread = read(src_fd[idx[i]], buf, BUF_LEN);
+          if (nread == -1) {
+            perror("read");
+            return 2;
+          }
+          if (nread == 0) {
+            closed[idx[i]] = 1;
+            break;
+          }
+          if (write_all(dst_fd[idx[i]], buf, nread) == -1) {
+            perror("write");
+            return 3;
+          }
+          if (nread < BUF_LEN) break;
         }
-
-        if (nread < BUF_LEN) break;
+      }
+      if (pollfds[i].revents & POLLHUP) {
+        closed[idx[i]] = 1;
       }
     }
   }
--- socket/a.c
+++ socket/a.c
@@ -1,16 +1,11 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
+#include <poll.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/select.h>
 #include <sys/socket.h>
 #include <sys/types.h>
 #include <unistd.h>
-
-int max(int a, int b) {
-  if (a > b) return a;
-  return b;
-}
 
 // call write syscall repeatedly until entire bytes are written.
 ssize_t write_all(int fd, char *buf, size_t count) {
@@ -29,64 +24,56 @@ int redirect(int sock) {
   const size_t BUF_LEN = 1024;
 
   char buf[BUF_LEN + 1];
-  fd_set readfds;
-  int stdin_closed = 0, sock_closed = 0;
+  struct pollfd pollfds[2];
+  int idx[2], src_fd[2], dst_fd[2], closed[2] = {0};
+  int nfd;
 
-  while (!sock_closed) {
-    FD_ZERO(&readfds);
-    FD_SET(STDIN_FILENO, &readfds);
-    FD_SET(sock, &readfds);
+  src_fd[0] = STDIN_FILENO;
+  dst_fd[0] = sock;
+  src_fd[1] = sock;
+  dst_fd[1] = STDOUT_FILENO;
 
-    int ret = select(max(STDIN_FILENO, sock) + 1, &readfds, NULL, NULL, NULL);
-    // when some error happen
-    if (ret == -1) {
-      perror("select");
-      return 1;
-    }
-
-    // read bytes from stdin and write them to sock
-    if (!stdin_closed && FD_ISSET(STDIN_FILENO, &readfds)) {
-      ssize_t nread, nwrite;
-      for (;;) {
-        nread = read(STDIN_FILENO, buf, BUF_LEN);
-        if (nread == -1) {
-          perror("read stdin");
-          return 2;
-        }
-        if (nread == 0) {  // EOF
-          stdin_closed = 1;
-          break;
-        }
-
-        if (write_all(sock, buf, nread) == -1) {
-          perror("write sock");
-          return 3;
-        }
-
-        if (nread < BUF_LEN) break;
+  while (!closed[1]) {
+    nfd = 0;
+    for (int i = 0; i < 2; i++) {
+      if (!closed[i]) {
+        pollfds[nfd].fd = src_fd[i];
+        pollfds[nfd].events = POLLIN;
+        pollfds[nfd].revents = 0;
+        idx[nfd] = i;
+        nfd += 1;
       }
     }
 
-    // read bytes from sock and write them to stdout
-    if (!sock_closed && FD_ISSET(sock, &readfds)) {
-      ssize_t nread, nwrite;
-      for (;;) {
-        nread = read(sock, buf, BUF_LEN);
-        if (nread == -1) {
-          perror("read sock");
-          return 2;
-        }
-        if (nread == 0) {  // EOF
-          sock_closed = 1;
-          break;
-        }
+    int ret = poll(pollfds, nfd, -1);
+    // when some error happen
+    if (ret == -1) {
+      perror("poll");
+      return 1;
+    }
 
-        if (write_all(STDOUT_FILENO, buf, nread) == -1) {
-          perror("write stdout");
-          return 3;
+    for (int i = 0; i < nfd; i++) {
+      if (!closed[idx[i]] && (pollfds[i].revents & POLLIN)) {
+        ssize_t nread, nwrite;
+        for (;;) {
+          nread = read(src_fd[idx[i]], buf, BUF_LEN);
+          if (nread == -1) {
+            perror("read");
+            return 2;
+          }
+          if (nread == 0) {
+            closed[idx[i]] = 1;
+            break;
+          }
+          if (write_all(dst_fd[idx[i]], buf, nread) == -1) {
+            perror("write");
+            return 3;
+          }
+          if (nread < BUF_LEN) break;
         }
-
-        if (nread < BUF_LEN) break;
+      }
+      if (pollfds[i].revents & POLLHUP) {
+        closed[idx[i]] = 1;
       }
     }
   }
