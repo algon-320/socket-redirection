53e86d32e29e11e15fe940ba49665e33eefc768e
algon-320 <algon.0320@gmail.com>
2020-08-08 22:43:39 +0900

    remove unnecessary includes

--- socket/a.c
+++ socket/a.c
@@ -1,11 +1,9 @@
 #include <arpa/inet.h>
-#include <fcntl.h>
 #include <netinet/in.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/select.h>
 #include <sys/socket.h>
-#include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
 
